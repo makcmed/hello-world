435ee2e66dc6be35aa136f84754fbc6b05fd6820
Maksim Medvedev <makcmed90@ya.ru>
2020-10-22 15:05:56 +0400

    java tutorial helloworld

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "World";
+
+        System.out.println(hello + " " + world + "!");
+
+    }
+}
